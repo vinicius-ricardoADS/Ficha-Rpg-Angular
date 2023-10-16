4ae4935ddf264d6ff4d7e0530bff3889ea9fe792
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-10-16 14:24:32 -0300

    feat: added route for register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent}
+  { path: '', component: HomePageComponent},
+  { path: 'register', component: RegisterPageComponent},
 ]
 
 @NgModule({
