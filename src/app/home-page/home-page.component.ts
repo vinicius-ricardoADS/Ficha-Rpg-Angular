f33d1e57f6c6acab1f00f555cb4bab572c8e6071
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-10-28 17:37:11 -0300

    feat: fetch chips in api using HttpClient package

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Chips } from './type';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
 
+  chips: Chips[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Chips[]>('http://localhost:3000/chips').subscribe(chips => {
+      this.chips = chips;
+    });
+  }
 }
