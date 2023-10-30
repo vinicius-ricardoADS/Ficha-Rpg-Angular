527943176c38837c3ad330e3df9d5e98c9d427d6
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-10-30 16:17:41 -0300

    merge branch 'feat-home-page' into feat-register-page

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './home-page/header/header.component';
 import { FormComponent } from './register-page/form/form.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { CardsComponent } from './home-page/cards/cards.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
